ef88c08d453e3052e667785b520ea53810dae572
StefanP <stefandev_mail@pm.me>
2022-03-15 19:13:03 +0100

    Limit height of card

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//https://javascript.tutorialink.com/automatically-close-all-the-other-tags-after-opening-a-specific-tag/
+
 const details = document.querySelectorAll("details");
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
